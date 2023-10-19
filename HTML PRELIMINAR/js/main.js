47c4b33259815016b965477c8633b9cddea42836
Gusty71 <144394992+Gusty71@users.noreply.github.com>
2023-10-19 17:42:07 -0300

    Update main.js
    es parte de js

--- HTML PRELIMINAR/js/main.js
+++ HTML PRELIMINAR/js/main.js
@@ -1,1 +1,17 @@
+document.getElementById("registartionform").addEventListener("submit", function(event){
+    event.preventDefault();     //evita que el formulario se envie automactic.
+    
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    //validacion basica del formulario
+    if (password !== confirmPassword){
+        alert("Las contarseñas no coinciden");
+        return;
+    }
+    alert("Registro exitoso");      //mensaje de texto
+    //restablece el formulario
+    document.getElementById("registrationForm").requestFullscreen();
+})
 
