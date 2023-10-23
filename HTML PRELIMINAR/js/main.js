6b61f1a8df34a02b1f362af6cd86e32578bfbff9
FlorPacialeo <florencia.pacialeo@gmail.com>
2023-10-23 12:34:36 -0300

    header y footer en JS

--- HTML PRELIMINAR/js/main.js
+++ HTML PRELIMINAR/js/main.js
@@ -1,4 +1,4 @@
-document.getElementById("registartionform").addEventListener("submit", function(event){
+/*document.getElementById("registartionform").addEventListener("submit", function(event){
     event.preventDefault();     //evita que el formulario se envie automactic.
     
     var username = document.getElementById("username").value;
@@ -13,5 +13,49 @@ document.getElementById("registartionform").addEventListener("submit", function(
     alert("Registro exitoso");      //mensaje de texto
     //restablece el formulario
     document.getElementById("registrationForm").requestFullscreen();
-})
+})*/
 
+let cadfooter=`
+<div class="textoredes"><h3>Nuestra Redes Sociales</h2></div>
+
+                <div class="redes">
+                    <div><a href="https://www.instagram.com">
+                        <img class="red" src="./imgs/instagram.png" alt=""></a>
+                    </div>
+                    <div><a href="https://www.facebook.com">
+                        <img class="red" src="./imgs/facebook.png" alt=""></a>
+                    </div>
+                    <div><a href="https://www.whatsapp.com">
+                        <img class="red" src="./imgs/whatsapp.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="domicilioycontacto">
+                    <p class="infofooter">
+                        Domicilio y contacto:
+                        telefono: 11 2890-4550
+                        Av. Saenz 420-
+                        C.P. 1900 BV -Cap. Fed.-
+                        Buenos Aires(Argentina)
+                    </p>  
+                </div>
+`
+document.querySelector("footer").innerHTML=cadfooter
+
+let cadheader=`
+<div class="contenedor-flex-navbar">
+<div class="nav-item" id="logo">ENGLISH INSTITUTE</div>
+<div class="flexmainhover nav-item">
+    <a class="cambiocolorletra" href="index.html">Home</a>
+</div>
+<div class="flexmainhover nav-item">
+    <a class="cambiocolorletra" href="sobrenosotros.html">Sobre nosotros</a>
+</div>
+<div class="flexmainhover nav-item">
+    <a class="cambiocolorletra" href="cursos.html">Cursos</a>
+</div>
+<div class="flexmainhover nav-item">
+    <a class="cambiocolorletra" href="contacto.html">Contacto</a>
+</div>
+</div>
+`
+document.querySelector("header").innerHTML=cadheader
